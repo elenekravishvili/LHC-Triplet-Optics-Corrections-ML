fc10b3db54b46b7b3283656fdf2409e2f4cc46d2
Elene Kravishvili <ekravish@cs-ccr-dev1.cern.ch>
2024-05-22 15:19:16 +0200

    test commit and include twiss recons with offset

--- recons_twiss.py
+++ recons_twiss.py
@@ -6,14 +6,49 @@ import os
 from pathlib import Path
 from joblib import load
 from cpymad import madx
+from sklearn.ensemble import RandomForestRegressor
 
 
-dpp_1= 1
-dpp_2 = 2
+def merge_data(data_path, noise, estimator):
+    #Takes folder path for all different data files and merges them
+    input_data, output_data = [], []
+    pathlist = Path(data_path).glob('**/*.npy')
+    file_names = [str(path).split('/')[-1][:-4] for path in pathlist]
+
+    for file_name in file_names:
+        beta_bpm_x_b1, beta_bpm_y_b1, beta_bpm_x_b2, beta_bpm_y_b2,\
+        input_data, output_data, dpp1, dpp_2 = load_data_mod(file_name, noise)
+        for i in range(1):
+            predicted_data = estimator.predict([input_data[i]])
+            corrected_errors = output_data[i]-predicted_data
+            print(corrected_errors.shape)
+
+    # return np.concatenate(input_data), np.concatenate(output_data)
+
+def load_data_mod(set_name, noise):
+    
+    all_samples = np.load('./data_include_offset/{}.npy'.format(set_name), allow_pickle=True)
+
+    delta_beta_star_x_b1, delta_beta_star_y_b1, delta_beta_star_x_b2, \
+        delta_beta_star_y_b2, delta_mux_b1, delta_muy_b1, delta_mux_b2, \
+            delta_muy_b2,\
+            beta_bpm_x_b1, beta_bpm_y_b1, beta_bpm_x_b2, beta_bpm_y_b2, \
+            triplet_errors, dpp_1, dpp_2 = all_samples.T
+    
+    input_data = np.concatenate( (np.vstack(delta_beta_star_x_b1), np.vstack(delta_beta_star_y_b1), \
+            np.vstack(delta_beta_star_x_b2), np.vstack(delta_beta_star_y_b2), \
+            np.vstack(delta_mux_b1), np.vstack(delta_muy_b1), \
+            np.vstack(delta_mux_b2), np.vstack(delta_muy_b2)), axis=1)   
+    
+    output_data = np.concatenate( (np.vstack(triplet_errors), np.vstack(dpp_1), np.vstack(dpp_2)), axis=1)
+    # betas = beta_bpm_x_b1, beta_bpm_y_b1, beta_bpm_x_b2, beta_bpm_y_b2
+
+    return np.vstack(beta_bpm_x_b1), np.vstack(beta_bpm_y_b1), np.vstack(beta_bpm_x_b2), np.vstack(beta_bpm_y_b2),\
+     input_data, output_data, dpp_1, dpp_2
 
 
 
-def recons_twiss( beam, mdx, dpp_1, dpp_2):
+def recons_twiss_with_off( beam, mdx, dpp_1, dpp_2):
     mdx.options(echo=False)
     mdx.call(file="/afs/cern.ch/eng/sl/lintrack/Beta-Beat.src/madx/lib/beta_beat.macros.madx")
     mdx.call(file="/afs/cern.ch/eng/sl/lintrack/Beta-Beat.src/madx/lib/lhc.macros.madx")
@@ -57,19 +92,17 @@ def recons_twiss( beam, mdx, dpp_1, dpp_2):
     
     mdx.input(f"""dpp_offset={dpp_1};
             exec, do_twiss_elements(LHCB1, "twiss_off.tfs", dpp_offset);
-                            
-            correct,mode=svd;  
-            exec, do_twiss_elements(LHCB1, "twiss_corrected.tfs", dpp_offset);            
-                        
-          
+            correct,mode=svd; 
+            exec, do_twiss_elements(LHCB1, "twiss_corrected.tfs", dpp_offset);  
             exec, find_complete_tunes(62.28, 60.31, 1);
-            exec, match, deltap=dpp_offset;
-            exec, vary, name=dQx.bbeam_number_op;
-            exec, vary, name=dQy.bbeam_number_op;
-            exec, constraint, range=#E, mux=nqx, muy=nqy;
-            exec, lmdif;
-            exec, endmatch;
-            exec, do_twiss_elements(LHCB1, "twiss_final.tfs", dpp_offset);
+            match, deltap=dpp_offset;
+            vary, name=dQx.b1_op;
+            vary, name=dQy.b1_op;
+            constraint, range=#E, mux=62.28, muy=60.31;
+            lmdif;
+            endmatch;
+            exec, do_twiss_elements(LHCB1, "twiss_corrected.tfs", dpp_offset);
+              
                """)
 
 
@@ -77,11 +110,7 @@ def recons_twiss( beam, mdx, dpp_1, dpp_2):
     #tfs.writer.write_tfs(tfs_file_path=f"final_errors.tfs", data_frame=mdx.table.final_error.dframe())
 
     # Generate twiss with columns needed for training data
-    mdx.input(f"""ndx := table(twiss,dx)/sqrt(table(twiss,betx));
-               select, flag=twiss, clear;
-              select, flag=twiss, pattern="^BPM.*B{beam}$", column=name, s, betx, bety, ndx,
-                                             mux, muy;
-           twiss, chrom, sequence=LHCB{beam}, deltap=0.0, file="";""")
+
 
     return mdx.table.twiss.dframe()
 
@@ -124,3 +153,13 @@ def job_energyoffset_b1(self, seed):
 
         ! twiss, chrom;                                    
     '''% {"SEED": seed})
+
+
+
+example = tfs.read("b1_nominal_monitors.dat").set_index("NAME")
+data_path = "data_include_offset"
+noise = 1E-4
+Nth_array=68
+mdx = madx.Madx()
+loaded_estimator = load('./estimators/test_2_triplet_phases_028_ridge_0.0001.pkl')
+merge_data(data_path, noise, loaded_estimator)
